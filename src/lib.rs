27a3a6af01b6ba0018dc1a563b297ddce5660a04
Alfan Nur Fauzan <alpancs@gmail.com>
2022-12-30 15:59:15 +0700

    handle telegram update

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,30 @@
+use serde::Deserialize;
 use worker::*;
 
 #[event(fetch)]
 pub async fn main(req: Request, env: Env, _ctx: worker::Context) -> Result<Response> {
     Router::new()
-        .get("/", |_, _| Response::ok("Hello from Workers!"))
+        .post_async("/webhook/telegram/update", |mut req, _ctx| async move {
+            match req.json::<Update>().await {
+                Ok(update) => handle_update(update),
+                Err(err) => Response::error(err.to_string(), 400),
+            }
+        })
         .run(req, env)
         .await
+}
+
+fn handle_update(update: Update) -> Result<Response> {
+    Response::ok(format!("{:?}", update))
+}
+
+#[derive(Deserialize, Debug)]
+struct Update {
+    inline_query: InlineQuery,
+}
+
+#[derive(Deserialize, Debug)]
+struct InlineQuery {
+    id: String,
+    query: String,
 }
