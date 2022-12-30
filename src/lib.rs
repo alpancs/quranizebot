20c15e131ad826cb49754747c2541a04b2e24974
Alfan Nur Fauzan <alpancs@gmail.com>
2022-12-30 15:08:09 +0700

    remove logger

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,7 @@
 use worker::*;
 
-fn log_request(req: &Request) {
-    console_log!(
-        "{} - [{}], located at: {:?}, within: {}",
-        Date::now().to_string(),
-        req.path(),
-        req.cf().coordinates().unwrap_or_default(),
-        req.cf().region().unwrap_or_else(|| "unknown region".into())
-    );
-}
-
 #[event(fetch)]
 pub async fn main(req: Request, env: Env, _ctx: worker::Context) -> Result<Response> {
-    log_request(&req);
     Router::new()
         .get("/", |_, _| Response::ok("Hello from Workers!"))
         .run(req, env)
